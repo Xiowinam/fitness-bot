fb8398b09f9de244482972a9da9e01423f0ea66a
unknown <kirill.goncharov.63@mail.ru>
2025-09-03 20:56:13 +1000

    commit12

--- FitnessBot/Program.cs
+++ FitnessBot/Program.cs
@@ -57,15 +57,38 @@ static string ConvertRenderDbUrlToConnectionString(string renderDbUrl)
 {
     try
     {
-        // Формат: postgresql://username:password@hostname:5432/database
+        Console.WriteLine($"Converting DB URL: {renderDbUrl}");
+
+        // Удаляем префикс postgresql:// если есть
+        if (renderDbUrl.StartsWith("postgresql://"))
+        {
+            renderDbUrl = renderDbUrl.Replace("postgresql://", "postgres://");
+        }
+
+        // Парсим URI
         var uri = new Uri(renderDbUrl);
         var userInfo = uri.UserInfo.Split(':');
 
-        return $"Host={uri.Host};Port={uri.Port};Username={userInfo[0]};Password={userInfo[1]};Database={uri.AbsolutePath.TrimStart('/')};SSL Mode=Require;Trust Server Certificate=true";
+        if (userInfo.Length != 2)
+        {
+            throw new FormatException("Invalid user info format in database URL");
+        }
+
+        var username = userInfo[0];
+        var password = userInfo[1];
+        var host = uri.Host;
+        var port = uri.Port;
+        var database = uri.AbsolutePath.TrimStart('/');
+
+        Console.WriteLine($"Parsed: Host={host}, Port={port}, User={username}, DB={database}");
+
+        // Формируем connection string
+        return $"Host={host};Port={port};Username={username};Password={password};Database={database};SSL Mode=Require;Trust Server Certificate=true";
     }
     catch (Exception ex)
     {
         Console.WriteLine($"Error converting database URL: {ex.Message}");
+        Console.WriteLine($"URL was: {renderDbUrl}");
         throw;
     }
 }
